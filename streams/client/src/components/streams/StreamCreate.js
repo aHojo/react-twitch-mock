e7fc443716b54269dda2a4fbc99e2641677327e1
ahojo <ahojodev@gmail.com>
2022-03-06 22:41:58 -0500

    Finished Auth

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,68 @@
 import React from "react";
+import { Field, reduxForm} from 'redux-form';
 
-const StreamCreate = () => {
-    return <div>StreamCreate</div>;
+class StreamCreate extends React.Component {
+
+    renderInput = (formProps) => {
+        // console.log(formProps);
+        // return <input onChange={formProps.input.onChange} value={formProps.input.value} name={formProps.input.name}/>
+        const className = `field ${formProps.meta.error && formProps.meta.touched ? 'error': ''}`
+        return (
+            <div className={className}>
+                <label htmlFor="">{formProps.label}</label>
+                <input {...formProps.input} autoComplete="off"/>
+                {this.renderError(formProps.meta)}
+            </div>
+        );
+    }
+
+    onSubmit(formValues) {
+        // formValues comes form redux-form
+        console.log(formValues)
+
+    }
+    renderError({error, touched}) {
+        if (touched && error) {
+            return (
+                <div className="ui error message">
+                    <div className="header">{error}</div>
+                </div>
+            );
+        }
+    }
+
+    render() {
+        console.log(this.props)
+        // this.props.handleSubmit is coming from redux-form
+        return (
+            <form className={`ui form error`} onSubmit={this.props.handleSubmit(this.onSubmit)}>
+                <Field name={`title`} label="Enter Title" component={this.renderInput} />
+                <Field name={`description`} label="Enter Description" component={this.renderInput} />
+                <button className="ui button primary">Submit</button>
+            </form>
+        );
+    }
 }
 
-export default StreamCreate;
+const validate = (formValues) => {
+    let errors = {};
+    if (!formValues.title) {
+        // only run if the user didn't enter a title
+        errors.title = 'You must enter a title';
+    }
+    if (!formValues.description) {
+        // only run if the user didn't enter a title
+        errors.description = 'You must enter a description';
+    }
+
+    // redux-form requires an emtpy object if no errors were found.
+    return errors;
+};
+
+
+export default reduxForm(
+    {
+        form: 'streamCreate',
+        validate,
+    }
+)(StreamCreate);
